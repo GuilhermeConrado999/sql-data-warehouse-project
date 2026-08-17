b16597e948b0b3748a454794b87171fb09cfeabb
Guilherme Conrado <128414841+GuilhermeConrado999@users.noreply.github.com>
2026-08-17 16:47:34 -0300

    Initialize 'DataWarehouse' database and schemas
    This script initializes the 'DataWarehouse' database by dropping it if it exists and recreating it along with its schemas.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,26 @@
+-- Create Database 'DataWarehouse'
 
+USE master;
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXITS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END
+GO
+
+-- Create the 'DataWarehouse' Database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
